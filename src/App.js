a5b8da2804bb379939ebe69a7d12002de3babf0b
Skeptecisme <fedikrayem@gmail.com>
2023-07-10 14:09:30 +0200

    editing files

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            <a href=".">ArtiSite, Where excellence meets digital excellence.</a>
           </div>
           <div className="social">
-            <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.facebook.com/artisite" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
-            <a href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/artisite_/" title="Twitter" target="_blank" rel="noopener noreferrer">
               <IconTwitter className="icon" />
             </a>
-            <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Skepticisme/" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
             </a>
           </div>
@@ -29,13 +29,13 @@ class App extends React.Component {
             <h1>Get ready for the change.</h1>
             <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
           </div>
-          <a href="mailto:user@example.com">
+          <a href="mailto:artisite@proton.me">
             <div className="cta">Send us an email</div>
           </a>
         </div>
         <div className="footer">
-          <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
+<span>made by <a className="underlined" href="https://github.com/Skepticisme/" target="_blank" rel="noopener noreferrer">ArtiSite</a>  <a className="underlined" href="https://www.legalzoom.com/articles/the-term-all-rights-reserved-explained" title="rights" target="_blank" rel="noopener noreferrer">All rights reserved </a> </span>
+</div>
       </div>
     );
   }
